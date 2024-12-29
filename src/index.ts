6ec21af968555b53c2cd6c3197d7142663129770
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2024-12-29 22:12:06 +0530

    cors added

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-client-glpk.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
